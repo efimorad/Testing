66da6556b5b545b70c7e7d1ed6d2077f38cdc278
Roei Kessler <roei@pcaid.co.il>
2013-11-15 18:08:29 +0200

    Best Code Ever

--- Testing/src/com/example/testing/MainActivity.java
+++ Testing/src/com/example/testing/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
+	//Al Hakefak
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
